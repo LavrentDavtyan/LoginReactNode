5608b57acd05030a65bfd29b38e19cc4e2fcc47b
Lavrent Davtyan <lavrentdavtyan1@gmail.com>
2024-08-09 14:00:30 +0400

    small fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-// const users = require('models/users.js');
 const users = require('./models/users');
 
 console.log(users)
